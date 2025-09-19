f3515966884697585cd979fcbcb4c44d978ceb9e
THEODORAANT <35761172+THEODORAANT@users.noreply.github.com>
2025-09-19 12:47:29 +0300

    Allow questionnaire question order to be managed

---
+++ perch/addons/apps/perch_members/modes/questions.edit.pre.php
@@ -0,0 +1,71 @@
+<?php
+    $message = false;
+    $Questions = new PerchMembers_QuestionnaireQuestions($API);
+
+    if (isset($_GET['id']) && $_GET['id']!='') {
+        $questionID = (int) $_GET['id'];
+        $Question = $Questions->find($questionID);
+        $details = $Question->to_array();
+        if (isset($details['options'])) {
+            $opts = PerchUtil::json_safe_decode($details['options'], true);
+            if (is_array($opts)) {
+                $lines = [];
+                foreach($opts as $k=>$v) $lines[] = $k.':'.$v;
+                $details['options'] = implode("\n", $lines);
+            }
+        }
+        $heading1 = $Lang->get('Editing question');
+    } else {
+        $Question = false;
+        $details = [];
+        $heading1 = $Lang->get('Creating question');
+    }
+
+    $Form = $API->get('Form');
+    $Form->require_field('label', 'Required');
+    $Form->require_field('questionKey', 'Required');
+
+    if ($Form->submitted()) {
+        $postvars = ['questionnaireType','questionKey','label','type','options','sort'];
+        $data = $Form->receive($postvars);
+        if (isset($data['options'])) {
+            $opts = preg_split('/\r\n|\r|\n/', trim($data['options']));
+            $json = [];
+            foreach($opts as $line) {
+                if ($line==='') continue;
+                $parts = explode(':', $line, 2);
+                if (count($parts)==2) {
+                    $json[$parts[0]] = $parts[1];
+                } else {
+                    $json[$line] = $line;
+                }
+            }
+            $data['options'] = json_encode($json);
+        }
+
+        if (!isset($data['sort']) || !is_numeric($data['sort'])) {
+            $data['sort'] = $Questions->get_next_sort_for_type($data['questionnaireType']);
+        } else {
+            $data['sort'] = (int)$data['sort'];
+            if ($data['sort'] < 0) {
+                $data['sort'] = $Questions->get_next_sort_for_type($data['questionnaireType']);
+            }
+        }
+
+        if ($Question) {
+            $Question->update($data);
+        } else {
+            $Question = $Questions->create($data);
+        }
+        $message = $HTML->success_message('Question has been successfully saved. Return to %squestion listing%s', '<a href="'.$API->app_path().'/questionnaire_questions/">', '</a>');
+        $details = $Question->to_array();
+        if (isset($details['options'])) {
+            $opts = PerchUtil::json_safe_decode($details['options'], true);
+            if (is_array($opts)) {
+                $lines = [];
+                foreach($opts as $k=>$v) $lines[] = $k.':'.$v;
+                $details['options'] = implode("\n", $lines);
+            }
+        }
+    }
+?>
---
+++ perch/addons/apps/perch_members/modes/questions.list.pre.php
@@ -0,0 +1,49 @@
+<?php
+    $HTML = $API->get('HTML');
+
+    $Questions = new PerchMembers_QuestionnaireQuestions($API);
+
+    $Paging = $API->get('Paging');
+    $Paging->set_per_page(20);
+
+    $Form = $API->get('Form');
+
+    if ($Form->submitted() && isset($_POST['btnSaveOrder'])) {
+        $items = $Form->find_items('order-');
+        $updated = false;
+
+        if (PerchUtil::count($items)) {
+            foreach ($items as $questionID => $order) {
+                $Question = $Questions->find((int)$questionID);
+                if (!$Question) continue;
+
+                $order = (int)$order;
+                if ($order < 0) {
+                    $order = $Questions->get_next_sort_for_type($Question->questionnaireType());
+                }
+
+                if ((int)$Question->sort() !== $order) {
+                    $Question->update(['sort' => $order]);
+                    $updated = true;
+                }
+            }
+        }
+
+        if ($updated) {
+            $message = $HTML->success_message($Lang->get('Question order has been updated.'));
+        }
+    }
+
+    $questions = $Questions->all();
+
+    $question_groups = [];
+    if (PerchUtil::count($questions)) {
+        foreach ($questions as $Question) {
+            $type = $Question->questionnaireType();
+            if (!isset($question_groups[$type])) {
+                $question_groups[$type] = [];
+            }
+            $question_groups[$type][] = $Question;
+        }
+    }
+?>
