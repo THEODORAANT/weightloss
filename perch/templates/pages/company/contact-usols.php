4d3277b462e2410414400dcef2a7a31a98cbb02a
theoMooblu <perchrunway@gmail.com>
2025-11-21 20:13:41 +0200

    weightloss

--- perch/templates/pages/company/contact-usols.php
+++ perch/templates/pages/company/contact-usols.php
@@ -1,40 +1,39 @@
+<?php  // output the top of the page
+perch_layout('global/header', [
+    'page_title' => perch_page_title(true),
+]);
 
-    <?php  // output the top of the page
-    perch_layout('global/header', [
-        'page_title' => perch_page_title(true),
-    ]);
-
-        /* main navigation
+/* main navigation
         perch_pages_navigation([
             'levels'   => 1,
             'template' => 'main_nav.html',
         ]);*/
 
-    ?>
-    <section class="research-section">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <div class="research">
-                        <span class="badge bg-dark">Contact Us</span>
-                        <h1 class="mt-3">Please email us for any questions you may have</h1>
-                        <p class="text-muted"><a href="mailto:support@getweightloss.co.uk"> support@getweightloss.co.uk</a></p>
-                    </div>
-                </div>
-                <div class="col-md-6 research_image ">
-
-                    <div class="research_img">
-                        <img src="/asset/contactus2.png" class="img-fluid" alt="Research Report">
-                    </div>
-
-
-
+?>
+<section class="research-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="research">
+                    <span class="badge bg-dark">Contact Us</span>
+                    <h1 class="mt-3">Please email us for any questions you may have</h1>
+                    <p class="text-muted"><a href="mailto:support@getweightloss.co.uk"> support@getweightloss.co.uk</a></p>
                 </div>
             </div>
-        </div>
-    </section>
+            <div class="col-md-6 research_image ">
 
-    <section class="custom" style="background-color: #f9f9fA;" >
+                <div class="research_img">
+                    <img src="/asset/contactus2.png" class="img-fluid" alt="Research Report">
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="custom" style="background-color: #f9f9fA;">
 
     <div class="container">
 
@@ -47,19 +46,18 @@
 
 
 
-    <ul class="accordion-list">
-    <?php
-        perch_collection('FAQS', [
+        <ul class="accordion-list">
+            <?php
+            perch_collection('FAQS', [
 
 
-            'count'      => 7,
-        ]);
-    ?>
-      </ul>
+                'count'      => 7,
+            ]);
+            ?>
+        </ul>
 
     </div>
 
 </section>
-      <?php
-           // perch_content('Intro');
-          perch_layout('global/footer');?>
+<?php //perch_content('Intro');
+perch_layout('global/new/footer'); ?>
