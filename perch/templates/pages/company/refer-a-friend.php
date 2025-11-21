e54d6eff9207b05bbf091a03a33e243acc1dbd08
THEODORAANT <35761172+THEODORAANT@users.noreply.github.com>
2025-11-21 18:31:55 +0200

    Ensure referral scheme list shows numbering

--- perch/templates/pages/company/refer-a-friend.php
+++ perch/templates/pages/company/refer-a-friend.php
@@ -59,38 +59,30 @@
 
 
                     <p>
-Refer a new patient (“New Patient”) to the GetWeightLoss site and you (“you”, “your” or “Referrer”) could each receive a discount on your next Programme payment (“Referral Scheme”) subject to the following terms and conditions:
+Refer a new patient (“New Patient”) to the GetWeightLoss site and you (“you” or “Referrer”) could receive a discount on your next Programme payment (“Referral Scheme”) subject to the following terms and conditions:
                    </p>
-                    <p>
- <p>1. You must be an existing patient of the Programme at the time of referring a New Patient to the Programme and you must be an existing patient of the Programme at the time the New Patient is accepted to the Programme;
- </p>
- <p>2. You must invite a New Patient to the Programme using your unique referral ID (“Referral ID”) shared with you. The Referral ID must only be used by you for the purposes of the Referral Scheme and must only be shared with a New Patient;
- </p>
- <p>3. The New Patient must be 18+ and reside in the United Kingdom;
- </p>
- <p>4. The New Patient must be accepted to the Programme following a clinical assessment by a GetWeightLoss clinician or successfully complete the online questionnaire;
- </p>
- <p>5. The New Patient cannot be an existing or previous patient;
- </p>
- <p>6. You can refer an unlimited number of New Patients to the Programme with one discount permitted per New Patient successfully referred. If you refer multiple New Patients, the discount will be applied automatically to each payment cycle until you have received a discount for each New Patient referred. If the Referral Payment is greater than the cost of the Weight Loss treatment the Referrer is using, the additional payment shall be held over until the following month;
- </p>
- <p>7. If a New Patient is successfully referred in accordance with these terms and conditions, the discount will be applied automatically from the New Patient’s first Programme payment.
- </p>
- <p>8. The Referral Scheme is intended to reward genuine referrals made in good faith to friends, family, or personal acquaintances. It is not intended for use in a commercial capacity or for any activity that generates profit or financial gain, beyond the benefits of the Referral Scheme. Participants in the Referral Scheme must not share, broadcast, or publish Referral Links on public forums, websites, or social media platforms intended for mass distribution.
- </p>
- <p>9. If we determine, at our sole discretion, that you or a New Patient have used the Referral Scheme in a manner that is inconsistent with or breaches these terms and conditions we reserve the right to revoke discounts, and suspend or terminate your account.
- </p>
- <p>10. The Referral Scheme cannot be used in conjunction with any other offer; and  </p>
-
- <p>11. The Referral Scheme is valid from April 2025 and may be withdrawn at any time.  </p>
-
-<p>12. The Referral Scheme is capped at 25 referrals in total per account.  </p>
-
- <p>13. The Referral Scheme Payout is as follows:
-	<ul><li>Referral First Purchase - £5</li>
-	<li> Referral Second Purchase - £10</li>
-	<li> Referral Third Purchase - £15</li></ul>
-                    </p>
+                    <ol style="list-style: decimal; padding-left: 20px;">
+                      <li>You must be an existing patient of the Programme at the time of referring a New Patient to the Programme and you must be an existing patient of the Programme at the time the New Patient is accepted to the Programme;</li>
+                      <li>You must invite a New Patient to the Programme using your unique referral ID (“Referral ID”) shared with you. The Referral ID must only be used by you for the purposes of the Referral Scheme and must only be shared with a New Patient;</li>
+                      <li>The New Patient must be 18+ and reside in the United Kingdom;</li>
+                      <li>The New Patient must be accepted to the Programme following a clinical assessment by a GetWeightLoss clinician or successfully complete the online questionnaire and then complete the purchase of a weight loss medication;</li>
+                      <li>The New Patient cannot be an existing or previous patient;</li>
+                      <li>You can refer an unlimited number of New Patients to the Programme with one discount permitted per New Patient successfully referred. If you refer multiple New Patients, the discount will be applied automatically to each payment cycle until you have received a discount for each New Patient referred. If the Referral Payment is greater than the cost of the Weight Loss treatment the Referrer is using, the additional payment shall be held over until the following month;</li>
+                      <li>If a New Patient is successfully referred in accordance with these terms and conditions, the discount will be applied automatically from the New Patient’s first Programme payment.</li>
+                      <li>The Referral Scheme is intended to reward genuine referrals made in good faith to friends, family, or personal acquaintances. It is not intended for use in a commercial capacity or for any activity that generates profit or financial gain, beyond the benefits of the Referral Scheme. Participants in the Referral Scheme must not share, broadcast, or publish Referral Links on public forums, websites, or social media platforms intended for mass distribution.</li>
+                      <li>If we determine, at our sole discretion, that you or a New Patient have used the Referral Scheme in a manner that is inconsistent with or breaches these terms and conditions we reserve the right to revoke discounts, and suspend or terminate your account.</li>
+                      <li>The Referral Scheme cannot be used in conjunction with any other offer; and </li>
+                      <li>The Referral Scheme is valid from August 2025 and may be withdrawn at any time.</li>
+                      <li>The Referral Scheme is uncapped but can only be used for payment towards your personal treatment through GetWeightLoss.co.uk.</li>
+                      <li>The Referral Scheme Payout is as follows:
+                        <ul>
+                          <li>referral fee £5.</li>
+                        </ul>
+                      </li>
+                      <li>Should you refer a patient who has previously been referred by another Referrer, the earliest registration of the referral will be the person they are attached to.</li>
+                      <li>Any referral who has not completed the process of consultation and purchase within 45 days of registration will be paid out at the discretion of GetWeightLoss.co.uk.</li>
+                      <li>GetWeightLoss.co.uk reserve the right to change these terms as and when they require, with the publication of such changes appearing in the Referral Scheme link on the website.</li>
+                    </ol>
 
 
                   </div>
