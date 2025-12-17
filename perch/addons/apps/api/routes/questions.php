2961aa26134ad3d642e6ffebba03b0d77343c7c2
theoMooblu <perchrunway@gmail.com>
2025-12-17 15:55:14 +0200

    weightloss

--- perch/addons/apps/api/routes/questions.php
+++ perch/addons/apps/api/routes/questions.php
@@ -9,6 +9,9 @@ $data = json_decode(file_get_contents('php://input'), true);
 $type = $data['type'] ?? 'first-order';
 $questions=perch_member_questionsForQuestionnaire($type);
 if($questions){
+if($type=="first-order"){
+$questions["conditions"]["label"]="Do any of the following statements apply to you?";
+}
     echo json_encode(["questions" => $questions]);
 } else {
     http_response_code(401);
